f0eb9ea8b6c2e6e1cfffe1dab892191bf17fec50
pqnzbmmwQ92 <104521533+pqnzbmmwQ92@users.noreply.github.com>
2022-07-05 07:25:46 +0300

    Add files via upload

--- program/downloader.py
+++ program/downloader.py
@@ -87,7 +87,7 @@ def song(_, message):
 
 
 @Client.on_message(
-    command(["vsong", f"vsong@{bn}", "video", f"فيديو"]) & ~filters.edited
+    command(["vsong", f"vsong@{bn}", f"تحميل_ف"]) & ~filters.edited
 )
 async def vsong(client, message):
     ydl_opts = {
