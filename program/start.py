9382e0d344e7fae5a2ff4ca42ee8e7ffb4772a7f
pqnzbmmwQ92 <104521533+pqnzbmmwQ92@users.noreply.github.com>
2022-07-05 07:26:32 +0300

    Update start.py

--- program/start.py
+++ program/start.py
@@ -58,7 +58,7 @@ async def _human_time_duration(seconds):
     return ", ".join(parts)
 
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
+@Client.on_message(command("/start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"{BOT_PHOTO}",
